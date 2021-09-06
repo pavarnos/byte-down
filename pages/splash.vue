fb1a06efc3737976cd39562d0a5ddec72088ee48
Steve Pavarno <pavarnos@gmail.com>
2021-09-06 12:52:44 +1200

    splash screen

---
+++ pages/splash.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="d-table" style="height:99vh">
+    <div class="d-table-cell align-middle">
+      <p class="text-center">
+        <b-link to="/"><img src="~static/ByteDownLogo.png" class="w-75" alt="ByteDown Logo"/></b-link>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script type="ts">
+export default {
+  layout: 'empty'
+}
+</script>
