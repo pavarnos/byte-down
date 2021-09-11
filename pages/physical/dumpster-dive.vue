3bceb454a372c33f6d6c956c099b6ab1167dfb04
Steve Pavarno <pavarnos@gmail.com>
2021-09-12 10:43:02 +1200

    add dumpster dive

---
+++ pages/physical/dumpster-dive.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Dumpster Dive</h1>
+    <a href="https://unsplash.com/@anniespratt" class="float-right">
+      <img src="~static/dumpster.jpg" alt="dumpster" width="400px"></a>
+    <p>Early next morning you turn up in some old clothes and walk quietly around the back of the factory. Nobody sees
+      you. Ah. There it is. A couple of skip bins that might have something useful inside.</p>
+    <p>You open the lid of the first one. It is full of dirty cartons and greasy industrial wrapping. No thanks. Not
+      looking in there. You close the lid before your nose tries to run off without you.</p>
+    <p>Lets try the second one. It's a bit whiffy but there is a pile of paper and cardboard inside. Hmmmm...</p>
+    <p>You start to rummage through, but quickly see that the paper is just wrapping material and has nothing useful
+      printed on it. You are looking for invoices, old printouts, customer lists, orders, meeting minutes, anything with
+      a few clues about what happens inside the building or who the customers are suppliers are</p>
+    <p>After a smelly 15 minutes you realise that The Dept must use a professional document destruction service for
+      their old office paper work. This is something they are doing right. Add a note to your report to congratulate
+      them.</p>
+    <p>Lets go home, have a shower and try something else.</p>
+    <BackButton />
+  </div>
+</template>
