58c248f455474aa99a8f04633adefa5979ea7ffb
pavarnos <pavarnos@users.noreply.github.com>
2021-09-15 05:09:41 +0000

    deploy: aecd0080fb9dafc1a7c4d4016d789054f1717732

---
+++ _nuxt/static/1631682576/splash/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fbyte-down\u002F_nuxt\u002Fstatic\u002F1631682576",layout:"empty",error:a,state:{holes:[]},serverRendered:true,routePath:"\u002Fsplash",config:{_app:{basePath:"\u002Fbyte-down\u002F",assetsPath:"\u002Fbyte-down\u002F_nuxt\u002F",cdnURL:a}}}}(null));
