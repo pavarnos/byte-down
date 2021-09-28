c1dbc9ec9df04653b51467339ccd3b2b17a6fb59
pavarnos <pavarnos@users.noreply.github.com>
2021-09-28 22:41:13 +0000

    deploy: cc5b6a2714cc239871b4184ddcab78a8e720d1a5

--- _nuxt/static/1632801062/network/web-server/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fbyte-down\u002F_nuxt\u002Fstatic\u002F1632801062",layout:"default",error:a,state:{holes:[],notes:[]},serverRendered:true,routePath:"\u002Fnetwork\u002Fweb-server",config:{_app:{basePath:"\u002Fbyte-down\u002F",assetsPath:"\u002Fbyte-down\u002F_nuxt\u002F",cdnURL:a}}}}(null));
