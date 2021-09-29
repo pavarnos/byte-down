824cdc3ba6c3282266961030d2fec0bf6840fcdb
pavarnos <pavarnos@users.noreply.github.com>
2021-09-29 03:10:09 +0000

    deploy: 6c83b54c2edfcb7e52429391f81425499e9d398e

--- _nuxt/static/1632872222/digital/phishing-send/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fbyte-down\u002F_nuxt\u002Fstatic\u002F1632872222",layout:"default",error:a,state:{holes:[],notes:[]},serverRendered:true,routePath:"\u002Fdigital\u002Fphishing-send",config:{_app:{basePath:"\u002Fbyte-down\u002F",assetsPath:"\u002Fbyte-down\u002F_nuxt\u002F",cdnURL:a}}}}(null));
