65232459fb393853f9976a4282e117d7b77eac18
pavarnos <pavarnos@users.noreply.github.com>
2021-10-04 03:29:45 +0000

    deploy: 1a123e796d5a96b1401a5cb3e353aeba3c7f80f4

---
+++ _nuxt/static/1633318171/physical/fake-delivery/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fbyte-down\u002F_nuxt\u002Fstatic\u002F1633318171",layout:"default",error:a,state:{holes:[],notes:[]},serverRendered:true,routePath:"\u002Fphysical\u002Ffake-delivery",config:{_app:{basePath:"\u002Fbyte-down\u002F",assetsPath:"\u002Fbyte-down\u002F_nuxt\u002F",cdnURL:a}}}}(null));
